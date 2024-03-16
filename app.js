fd5ff08e4915e46ee5d4575954bd98d37802647e
olearybm <90865347+olearybm@users.noreply.github.com>
2024-03-16 16:23:45 -0400

    Added some cleanup

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// Challenge #1
+// *********************************************************
+// Challenge #1 ********************************************
+// *********************************************************
 // Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
 // Variation 1: BMI = mass / height ** 2
 // Variation 2: BMI = mass / (height * height) 
@@ -14,7 +16,8 @@
 // - Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall
 // - Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall
 // *********************************************************
-// Challenge #2
+// Challenge #2 ********************************************
+// *********************************************************
 // Use the BMI example from Challenge #1, and the code you already wrote, and improve it
 
 // Your tasks:
@@ -29,6 +32,7 @@
 // }
 // Adding arrow function
 // const bmi = (mass, height) => mass / height ** 2;
+// *********************************************************
 
 //Mark
 // let markMass = 78;
@@ -61,7 +65,8 @@
 // }
 
 // *********************************************************
-// // Challenge #3
+// Challenge #3 ********************************************
+// *********************************************************
 // There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
 
 // Your tasks:
@@ -78,14 +83,17 @@
 // - Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
 // - Data for Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123 
 // - Data for Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+// *********************************************************
 
 // function avgScore(scoreArray){
 //     let totalScores = 0;
 
-//     for(i = 0; i < scoreArray.length; i++){
+//     for(i = 0; i < scoreArray.length; i++){'
+//         // Add all scores
 //         totalScores += scoreArray[i]
 //     }
 //     console.log(totalScores);
+//     // Average = the total divided by number of scores
 //     return totalScores / scoreArray.length
 // }
 // First data set:
@@ -101,7 +109,9 @@
 // let dolphinsAvgScore = avgScore(dolphinScores);
 // let koalasAvgScore = avgScore(koalasScores);
 
+// Only average scores above 100 can win trophy
 // if (dolphinsAvgScore > 100 || koalasAvgScore > 100){
+//     // Determine winner by score         
 //     if(dolphinsAvgScore > koalasAvgScore) {
 //         console.log(`Dolphins (${dolphinsAvgScore}) win over the Koalas (${koalasAvgScore})`);
 //     } else if (koalasAvgScore > dolphinsAvgScore) {
@@ -114,7 +124,8 @@
 // }
 
 // *********************************************************
-// Challenge #4
+// Challenge #4 ********************************************
+// *********************************************************
 // Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 
 // Your tasks:
@@ -128,12 +139,15 @@
 // - To calculate 20% of a value, simply multiply it by 0.2 (Why 0.2? Because 20 divided by 100 is equal to 0.2) 
 // - Value X is between 50 and 300 if it's >= 50 && <= 300
 
+// Data
 // let totalBill = 55;
 // let totalBill = 275;
 // let totalBill = 40;
 let totalBill = 430;
 
+// Ternary operator - tip variable
 const tip = (totalBill >= 50 && totalBill <= 300) ? .15 : .2;
+// The total value is the total paid with the total bill plus tip
 let totalValue = totalBill + (totalBill * tip);
 
 console.log(`The bill was \$${totalBill}, the tip was \$${totalBill * tip} at ${tip*100}%, and the total value \$${totalValue} `);
