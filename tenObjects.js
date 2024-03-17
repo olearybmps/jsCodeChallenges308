4622ff98bf10f375e230cb6814d716a01e72c515
olearybm <90865347+olearybm@users.noreply.github.com>
2024-03-17 16:03:59 -0400

    Added js file with 10 objects

---
+++ tenObjects.js
@@ -0,0 +1,150 @@
+let players1992DreamTeam = [
+    {
+        name: "Michael Jordan",
+        age: 29,
+        college: "University of North Carolina",
+        proTeam: "Chicago Bulls",
+        shooting: 95,
+        ballHandling: 90,
+        blockingShots: 75,
+        steals: 85,
+        freeThrowShooting: 85,
+        dunking: 99
+    },
+    {
+        name: "Magic Johnson",
+        age: 32,
+        college: "Michigan State University",
+        proTeam: "Los Angeles Lakers",
+        shooting: 88,
+        ballHandling: 95,
+        blockingShots: 70,
+        steals: 90,
+        freeThrowShooting: 90,
+        dunking: 80
+    },
+    {
+        name: "Larry Bird",
+        age: 35,
+        college: "Indiana State University",
+        proTeam: "Boston Celtics",
+        shooting: 90,
+        ballHandling: 85,
+        blockingShots: 65,
+        steals: 80,
+        freeThrowShooting: 95,
+        dunking: 75
+    },
+    {
+        name: "Charles Barkley",
+        age: 29,
+        college: "Auburn University",
+        proTeam: "Philadelphia 76ers",
+        shooting: 85,
+        ballHandling: 80,
+        blockingShots: 70,
+        steals: 80,
+        freeThrowShooting: 75,
+        dunking: 90
+    },
+    {
+        name: "Patrick Ewing",
+        age: 29,
+        college: "Georgetown University",
+        proTeam: "New York Knicks",
+        shooting: 80,
+        ballHandling: 70,
+        blockingShots: 95,
+        steals: 70,
+        freeThrowShooting: 80,
+        dunking: 85
+    },
+    {
+        name: "Scottie Pippen",
+        age: 26,
+        college: "University of Central Arkansas",
+        proTeam: "Chicago Bulls",
+        shooting: 85,
+        ballHandling: 85,
+        blockingShots: 80,
+        steals: 90,
+        freeThrowShooting: 85,
+        dunking: 90
+    },
+    {
+        name: "David Robinson",
+        age: 27,
+        college: "United States Naval Academy",
+        proTeam: "San Antonio Spurs",
+        shooting: 80,
+        ballHandling: 75,
+        blockingShots: 95,
+        steals: 75,
+        freeThrowShooting: 85,
+        dunking: 90
+    },
+    {
+        name: "Karl Malone",
+        age: 29,
+        college: "Louisiana Tech University",
+        proTeam: "Utah Jazz",
+        shooting: 85,
+        ballHandling: 80,
+        blockingShots: 75,
+        steals: 80,
+        freeThrowShooting: 80,
+        dunking: 90
+    },
+    {
+        name: "John Stockton",
+        age: 30,
+        college: "Gonzaga University",
+        proTeam: "Utah Jazz",
+        shooting: 85,
+        ballHandling: 90,
+        blockingShots: 65,
+        steals: 95,
+        freeThrowShooting: 90,
+        dunking: 70
+    },
+    {
+        name: "Chris Mullin",
+        age: 29,
+        college: "St. John's University",
+        proTeam: "Golden State Warriors",
+        shooting: 90,
+        ballHandling: 85,
+        blockingShots: 65,
+        steals: 75,
+        freeThrowShooting: 90,
+        dunking: 70
+    },
+    {
+        name: "Clyde Drexler",
+        age: 30,
+        college: "University of Houston",
+        proTeam: "Portland Trail Blazers",
+        shooting: 85,
+        ballHandling: 85,
+        blockingShots: 75,
+        steals: 85,
+        freeThrowShooting: 85,
+        dunking: 95
+    },
+    {
+        name: "Christian Laettner",
+        age: 22,
+        college: "Duke University",
+        proTeam: "Minnesota Timberwolves",
+        shooting: 75,
+        ballHandling: 70,
+        blockingShots: 80,
+        steals: 70,
+        freeThrowShooting: 80,
+        dunking: 80
+    }
+];
+
+// Test:
+console.log(players1992DreamTeam[0].name); // Output: "Michael Jordan"
+console.log(players1992DreamTeam[1].college); // Output: "Michigan State University"
